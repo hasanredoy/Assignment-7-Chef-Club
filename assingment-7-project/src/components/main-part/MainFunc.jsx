dd2f71f51c2dddaff07411d094974fcc168109c3
unknown <redoyhasan4585@gmail.com>
2024-03-16 10:55:21 +0400

    added side card and footer

--- assingment-7-project/src/components/main-part/MainFunc.jsx
+++ assingment-7-project/src/components/main-part/MainFunc.jsx
@@ -1,21 +1,23 @@
 import PropTypes from 'prop-types';
+import { IoMdTime } from "react-icons/io";
+import { RiFireLine } from "react-icons/ri";
 
 const MainFunc = ({ cards }) => {
   console.log(cards);
   return (
-    <div className=' grid grid-cols-1 lg:grid-cols-2 gap-4 w-[65%] '>
+    <div className=' grid grid-cols-1 lg:grid-cols-2 gap-4 mx-auto w-[95%] lg:w-[65%] '>
       {
         cards.map(card => {
            return (
             <div key={card.recipe_id} className="card  bg-base-200 shadow-xl p-4 border border-gray-400">
-              <figure><img className=' h-[252px]' src={card.recipe_image} alt="Shoes" /></figure>
+              <figure><img className=' rounded-2xl h-[252px] w-full' src={card.recipe_image} alt="Shoes" /></figure>
               <div className="card-body">
-                <h2 className="card-title font-bold">{card.recipe_name}</h2>
+                <h2 className="text-2xl font-bold">{card.recipe_name}</h2>
                 <p>{card.short_description}</p>
                 <div className="divider"></div>
                 <div className=''>
-                <h2 className="card-title font-bold">Ingredients: 6</h2>
-                <ul className=' list-disc'>
+                <h2 className=" text-2xl font-bold">Ingredients: 6</h2>
+                <ul className=' list-disc pl-6 text-lg'>
                   <li>{card.ingredients[0]}</li>
                   <li>{card.ingredients[1]}</li>
                   <li>{card.ingredients[2]}</li>
@@ -25,8 +27,9 @@ const MainFunc = ({ cards }) => {
                 </ul>
                 </div>
                 <div className=" divider"></div>
-                <div>
-                  
+                <div className='flex justify-between'>
+                  <h3 className='flex text-lg gap-2'><span className=' mt-1'><IoMdTime /></span>{card.preparing_time} minutes </h3>
+                  <h3 className='flex text-lg gap-2'><span className=' mt-1'><RiFireLine /></span>{card.calories} calories </h3>
                 </div>
                 <div className="card-actions justify-start">
                   <button className=" rounded-full btn bg-[#0BE58A]">Want To Cook</button>
--- assingment-7-project/src/components/sidebar/Sidebar.jsx
+++ assingment-7-project/src/components/sidebar/Sidebar.jsx
@@ -3,8 +3,29 @@ import PropTypes from 'prop-types';
 
 const Sidebar = ({sidebar}) => {
   return (
-    <div className=' w-[40%] bg-slate-300'>
-      <h1>hello from side bar</h1>
+    <div className=' w-[90%] mt-10 lg:mt-0 mx-auto lg:w-[40%] bg-gray-100 rounded-xl border border-gray-600 h-full'>
+      <h1 className=' text-center pt-2 lg:pt-5 font-bold text-2xl lg:text-4xl'>Want To Cook: <span>0</span></h1>
+      <div className="divider"></div>
+      <div className=' pr-12 flex justify-evenly'>
+        <h3>Name </h3> 
+        <h3>Time </h3> 
+        <h3>Calories </h3> 
+      </div>
+      <div>
+
+      </div>
+      <h1 className=' text-center pt-2 lg:pt-10 font-bold text-2xl lg:text-4xl'>Currently Cooking: <span>0</span></h1>
+      <div className="divider"></div>
+      <div className=' flex justify-evenly'>
+        <h3>Name </h3> 
+        <h3>Time </h3> 
+        <h3>Calories </h3> 
+      </div>
+      <div className="divider"></div>
+      <div className=' flex justify-evenly'>
+        <h3>Total Time=  </h3>  
+        <h3>Total Calories= </h3> 
+      </div>
     </div>
   );
 };
